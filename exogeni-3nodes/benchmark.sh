eb4f8f0a04b1a853fd826b461ae34759b62ba9c2
AXinx <xin-yue-2009@163.com>
2021-03-09 15:19:52 +0100

    change file name

--- exogeni-3nodes/benchmark.sh
+++ exogeni-3nodes/benchmark.sh
@@ -15,5 +15,5 @@ scp -r /root/caliper-bench/exogeni-3nodes/caliper-benchmarks/networks/fabric/fab
 cd /root/caliper-bench/exogeni-3nodes/monitor
 ./start.sh
 
-cd ../caliper-benchmarks-ben/
+cd ../caliper-benchmarks/
 npx caliper benchmark run --caliper-benchconfig benchmarks/samples/fabric/marbles/config.yaml --caliper-networkconfig networks/fabric/fabric-v1.4.1/swarm-3org1peer-raft/fabric-ccp-go-tls.yaml --caliper-workspace .
